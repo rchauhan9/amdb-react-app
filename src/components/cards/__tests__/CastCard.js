b5106f6d0288a1ca9bdf58edbe318c84aec34388
Rajan <rajan.chauhan18@imperial.ac.uk>
2021-01-18 20:35:29 +0000

    Fixed failing tests

--- src/components/cards/__tests__/CastCard.js
+++ src/components/cards/__tests__/CastCard.js
@@ -54,7 +54,7 @@ describe("<CastCard />", () => {
                 <CastCard castMember={castMember} />
             </Router>
         );
-        const cardTitle = wrapper.find("div.cast-actor-name")
+        const cardTitle = wrapper.find("div.card-title")
         expect(cardTitle).toHaveText("Christian Bale")
     })
 
@@ -64,7 +64,7 @@ describe("<CastCard />", () => {
                 <CastCard castMember={castMember} />
             </Router>
         );
-        const cardText = wrapper.find("p.cast-character")
+        const cardText = wrapper.find("p.card-text")
         expect(cardText).toHaveText("Bruce Wayne / Batman")
     })
 
