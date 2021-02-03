75d879c4017c7633e5505b306b58fd4f82b04c97
Rajan <rajan.chauhan18@imperial.ac.uk>
2021-02-03 10:26:25 +0000

    Added image to home view with url to private s3

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -17,6 +17,7 @@ const Home = () => {
                 <HomeCol>
                     <h1>What's New</h1>
                     <HScrollView>
+                        <img src="https://test-amdb.s3.eu-west-2.amazonaws.com/dark-knight.jpg" alt="Dark knight image was supposed to be here" />
                         <ImageProfileView src={PRESIGNED_URLS_TITLE + "17874e6d-ddd6-479a-9057-1b655e060bb6" + PROFILE} style={{margin: '20px'}}/>
                         <ImageProfileView src={PRESIGNED_URLS_TITLE + "17874e6d-ddd6-479a-9057-1b655e060bb6" + PROFILE} style={{margin: '20px'}}/>
                         <ImageProfileView src={PRESIGNED_URLS_TITLE + "17874e6d-ddd6-479a-9057-1b655e060bb6" + PROFILE} style={{margin: '20px'}}/>
